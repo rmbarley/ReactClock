92376a1e605a2d020db45674c5c8b071a108b00c
Ryan <rmbarley@gmail.com>
2016-12-08 23:03:59 -0500

    Create Clock and Nav components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,9 +8,8 @@ var Nav = () => {
           <ul className="menu">
             <li className="menu-text">React Time App</li>
             <li><IndexLink to="/" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Timer</IndexLink></li>
-            <li><Link to="/" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Countdown</Link></li>
-
-            </ul>
+            <li><Link to="/countdown" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Countdown</Link></li>
+          </ul>
         </div>
         <div className="top-bar-right">
           <ul className="menu">
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function () {
+    return <h4>Timer.jsx</h4>
+  }
+});
+
+module.exports = Timer;
